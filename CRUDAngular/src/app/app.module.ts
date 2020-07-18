50a3695176385a20903da2850b57d02e1fd208f1
shreebharaath <shreebharaath@hotmail.com>
2020-07-19 00:43:15 +0530

    update

---
+++ CRUDAngular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
+import { PaymentDetailListComponent } from './payment-details/payment-detail-list/payment-detail-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PaymentDetailService } from './shared/payment-detail.service';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailComponent,
+    PaymentDetailListComponent
+  ],
+  imports: [HttpClientModule,FormsModule,
+    BrowserModule
+  ],
+  providers: [PaymentDetailService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ CRUDAngular/src/app/payment-details/payment-detail/payment-detail.component.ts
@@ -0,0 +1,42 @@
+import { PaymentDetailService } from './../../shared/payment-detail.service';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-payment-detail',
+  templateUrl: './payment-detail.component.html',
+  styles: []
+})
+export class PaymentDetailComponent implements OnInit {
+
+  constructor(public service: PaymentDetailService) { }
+
+  ngOnInit() {
+    this.resetForm();
+  }
+  
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.form.reset();
+    this.service.formData = {
+      PMId: 0,
+      CardOwnerName: '',
+      CardNumber: '',
+      ExpirationDate: '',
+      CVV: ''
+    }
+  }  
+  onSubmit(form: NgForm) {
+    this.insertRecord(form);
+  }
+  
+  insertRecord(form: NgForm) {
+    this.service.postPaymentDetail().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList();
+      },
+      err => { console.log(err); }
+    )
+  }
+}
